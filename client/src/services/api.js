54415f33f501a80eca8b39507765768e99305df9
Avik Chakraborty <120381343+Avikchak3@users.noreply.github.com>
2026-05-10 20:52:13 +0530

    Fixed CORS issue

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,22 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    "https://expense-tracker-backend-nomn.onrender.com/api",
+});
+
+API.interceptors.request.use((req) => {
+
+  const token =
+    localStorage.getItem("token");
+
+  if (token) {
+
+    req.headers.Authorization =
+      `Bearer ${token}`;
+  }
+
+  return req;
 });
 
 export default API;
--- server/server.js
+++ server/server.js
@@ -21,7 +21,18 @@ const app = express();
 
 
 // MIDDLEWARES
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: [
+      "GET",
+      "POST",
+      "PUT",
+      "DELETE",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
